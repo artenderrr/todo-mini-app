45daa9346502ded5106a10663f87b4be56ed1e3d
artender <artem1347929@gmail.com>
2025-03-10 16:56:11 +0300

    ✨ Add skeleton layout for Home screen

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,3 +1,28 @@
+<script setup>
+import AppHeader from "./AppHeader.vue";
+import TodoPanel from "./TodoPanel.vue";
+</script>
+
 <template>
-  <p>Home</p>
+  <div class="wrapper">
+    <div class="app-header-wrapper">
+      <AppHeader />
+    </div>
+    <TodoPanel />
+  </div>
 </template>
+
+<style scoped>
+.wrapper {
+  height: 100%;
+  width: 100%;
+}
+
+.app-header-wrapper {
+  height: 20%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ frontend/src/components/TodoPanel.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const isHidden = ref(true);
+
+function slideIn() {
+  setTimeout(() => isHidden.value = false, 1);
+}
+
+onMounted(slideIn);
+</script>
+
+<template>
+  <div class="panel" :class="{ hidden: isHidden }"></div>
+</template>
+
+<style scoped>
+.panel {
+  background-color: #2b2b2b;
+
+  width: 40rem;
+  max-width: 100%;
+  height: 100%;
+
+  border: .125rem solid #3b3b3b;
+  border-bottom: none;
+  border-top-left-radius: 2rem;
+  border-top-right-radius: 2rem;
+
+  margin: auto;
+
+  transition: transform 1s;
+}
+
+.panel.hidden {
+  transform: translateY(100%);
+}
+</style>
